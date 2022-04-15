3b353fbaa498964cbad48a40e383c04f50652011
cygnet <bloopsocket@gmail.com>
2022-04-15 14:37:49 -0500

    restructure

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3 test 4
+            Learn React 3 test 5
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>

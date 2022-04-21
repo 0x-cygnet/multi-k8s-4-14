737fe775a9b6c80588ec32f045ea6dfc82858d60
cygnet <bloopsocket@gmail.com>
2022-04-21 17:30:39 -0500

    test fed

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3 test 6 Federation
+            Learn React 3 test 7 Federation
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>

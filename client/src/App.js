a0e1423b6a06e768bf487880e37351c546a12692
cygnet <bloopsocket@gmail.com>
2022-04-25 13:14:46 -0500

    test order

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3 test 9 https
+            Learn React 3 test 10 order
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>

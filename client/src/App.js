72c8234993ceef0787cf02759257280ec21f7440
0x-cygnet <bob@cygnetops.com>
2023-09-28 15:52:45 -0500

    test 3

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 4 test 1
+            Learn React 4 test 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
